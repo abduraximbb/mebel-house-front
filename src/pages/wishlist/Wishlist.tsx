7a4eea735bee5a899592b7390717a003edaf2db0
Abduraxim Berdiqobilov <abduraximberdiqobilov@Abduraxims-MacBook-Air.local>
2025-01-27 19:50:11 +0500

    Wishlist and EmptyWishList connected

--- src/pages/wishlist/Wishlist.tsx
+++ src/pages/wishlist/Wishlist.tsx
@@ -1,22 +1,30 @@
-import Products from '@/components/products/Products'
-import { RootState } from '@/redux'
-import { useGetWishlistQuery } from '@/redux/api/wishlist-api'
-import { useSelector } from 'react-redux'
+import Products from "@/components/products/Products";
+import { RootState } from "@/redux";
+import { useGetWishlistQuery } from "@/redux/api/wishlist-api";
+import { useSelector } from "react-redux";
+import EmptyWishlist from "./EmptyWishlist";
 
 const Wishlist = () => {
-    const id = useSelector((state: RootState)=> state.user.value.id)
-    const wishlist = useSelector((state: RootState)=> state.wishlist.value)
-    const token = useSelector((state: RootState)=> state.token.access_token)
-    
-    const {data} = useGetWishlistQuery(String(id))
-    console.log({data: wishlist});
-    
-    
+  const id = useSelector((state: RootState) => state.user.value.id);
+  const wishlist = useSelector((state: RootState) => state.wishlist.value);
+  const token = useSelector((state: RootState) => state.token.access_token);
+
+  const { data } = useGetWishlistQuery(String(id));
+  console.log({ data: wishlist });
+
   return (
     <div>
-        <Products data={token ? data : {data: wishlist}} showMore={true} title="Your Wishlist"/>
+      {wishlist?.length ? ( // wishlist mavjud bo'lsa
+        <Products
+          data={token ? data : { data: wishlist }} // Agar token bo'lsa, API ma'lumotlari, aks holda lokal wishlist
+          showMore={true} // Show More tugmasi faollashtirilgan
+          title="Your Wishlist" // "Your Wishlist" sarlavhasi
+        />
+      ) : (
+        <EmptyWishlist /> // Wishlist mavjud bo'lmasa, EmptyWishlist ko'rsatiladi
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Wishlist
+export default Wishlist;
