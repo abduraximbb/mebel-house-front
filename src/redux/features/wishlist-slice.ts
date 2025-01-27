6447a21c3b4d739a6ee9121bb670235c4b584fee
Abduraxim Berdiqobilov <abduraximberdiqobilov@Abduraxims-MacBook-Air.local>
2025-01-27 14:28:31 +0500

    Conflicts fixed in Signin, wishlist-slice and wishlist-api

--- src/redux/features/wishlist-slice.ts
+++ src/redux/features/wishlist-slice.ts
@@ -1,29 +1,33 @@
 import { IProduct } from "@/types";
-import { getStorage, saveStorage } from "@/utils";
+import { saveStorage } from "@/utils";
 import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
 
 export interface WishlistState {
-  value: IProduct[]
+  value: IProduct[];
 }
 
-const wishlistStore = localStorage.getItem("wishlist")
+const wishlistStore = localStorage.getItem("wishlist");
 const initialState: WishlistState = {
-  value: wishlistStore ? JSON.parse(wishlistStore) : []
-}
+  value: wishlistStore ? JSON.parse(wishlistStore) : [],
+};
 
 export const wishlistSlice = createSlice({
   name: "wishlist",
   initialState,
   reducers: {
     toggleLike: (state, action: PayloadAction<IProduct>) => {
-        const index = state.value.findIndex((item) => item.id === action.payload.id)
-        if(index < 0){
-            state.value = [...state.value, action.payload];
-        }else{
-            state.value = state.value.filter((item)=> item.id !== action.payload.id)
-        }
-        saveStorage("wishlist", state.value)
+      const index = state.value.findIndex(
+        (item) => item.id === action.payload.id
+      );
+      if (index < 0) {
+        state.value = [...state.value, action.payload];
+      } else {
+        state.value = state.value.filter(
+          (item) => item.id !== action.payload.id
+        );
+      }
+      saveStorage("wishlist", state.value);
     },
   },
 });
