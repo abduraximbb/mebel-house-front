219281a5242a4c03eb549027049213c112ac6c4b
yavina0810 <163087350+yavina0810@users.noreply.github.com>
2025-01-28 21:05:50 +0500

    fixed

--- src/pages/detail/MainDetail.tsx
+++ src/pages/detail/MainDetail.tsx
@@ -2,13 +2,15 @@ import ProductDetail from "./ProductDetail";
 import { useGetProductsQuery } from "@/redux/api/product-api";
 import Products from "@/components/products/Products";
 
+
 const MainDetail = () => {
   const { data, isLoading, isError } = useGetProductsQuery({ limit: 10000 });
 
   return (
     <>
-      <ProductDetail />
+      <ProductDetail /> 
       {!isLoading && !isError && data && <Products data={data}/>}
+    
     </>
   );
 };
